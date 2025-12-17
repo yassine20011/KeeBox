425e8b4bac03d71f8558e54cec729c411eaf3d3b
Youness SBAI <youness.sbai.work@gmail.com>
2025-12-17 15:46:21 +0100

    Cleared the whole project, cause i'm changing all this sh*t to Qt.

--- build-MinGW-W64.bat
+++
@@ -1,5 +0,0 @@
-cd build
-premake5.exe gmake
-cd ..
-mingw32-make clean
-pause
--- include/config.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-// Application window title
-#define WINDOW_TITLE "KeeBox"
-
-// Default window size settings
-#define DEFAULT_WINDOW_WIDTH  800  // Initial width of the window
-#define DEFAULT_WINDOW_HEIGHT 800  // Initial height of the window
